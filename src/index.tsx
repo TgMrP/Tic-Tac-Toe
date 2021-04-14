5b3d946c612368d67ff7d9316318a8bca1606396
TgMrP <tgmrp.dev@gmail.com>
2021-04-14 18:17:30 +0300

    start commiting

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from "react";
+import { StyleSheet, View } from "react-native";
+
+import { Text, AppBootstrap } from "@components";
+
+export default function App(): ReactElement {
+    return (
+        <AppBootstrap>
+            <View style={styles.container}>
+                <Text
+                    onPress={() => {
+                        alert(true);
+                    }}
+                    style={{ fontSize: 25 }}
+                >
+                    Hello World
+                    <Text weight="700">Child</Text>
+                </Text>
+            </View>
+        </AppBootstrap>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
